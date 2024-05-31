0ced479bc2822f6a210ae400f36cd0531dde40c0
Yahya Poursoltani <yahya.poursoltani@gmail.com>
2024-05-31 21:43:54 +0330

    create SVR Mutants

---
+++ src/edu/sharif/test/mutationPrac/mutants/ScalarVariableReplacement/MU_10_SVR_03.java
@@ -0,0 +1,29 @@
+package edu.sharif.test.mutationPrac.mutants.ScalarVariableReplacement;
+
+public class MU_10_SVR_03 {
+    /*
+    * Effects
+    *Val -> j
+    * */
+    public static int[] deleteAllVal(int[] numbers, int val1)
+    {
+        int[] remnum = new int[numbers.length];
+        if(numbers==null){
+            return numbers;
+        }
+        if(numbers.length==0){
+            return numbers;
+        }
+
+        int len= numbers.length;
+        int j=0;
+        for(int i=0; i<len; i++)
+            if(numbers[i]>=j) //TODO FAULTY INSTRUCTION
+            {
+                remnum[j] = numbers[i];
+                j=j+1;
+            }
+
+        return remnum;
+    }
+}
